f5cfefd024460a88852f87e6fa8c7b76c13ada58
Rene Arias <rene_811@hotmail.com>
2019-08-26 23:59:40 -0500

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyACL5HeRA8eZJk4eTTEjcExosYnZdvAKSg',
+    authDomain: 'pidelo-773e5.firebaseapp.com',
+    databaseURL: 'https://pidelo-773e5.firebaseio.com',
+    projectId: 'pidelo-773e5',
+    functionsURL: 'https://us-central1-pidelo-773e5.cloudfunctions.net',
+    storageBucket: 'pidelo-773e5.appspot.com',
+    messagingSenderId: '262600937391'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyACL5HeRA8eZJk4eTTEjcExosYnZdvAKSg',
+    authDomain: 'pidelo-773e5.firebaseapp.com',
+    databaseURL: 'https://pidelo-773e5.firebaseio.com',
+    projectId: 'pidelo-773e5',
+    functionsURL: 'https://us-central1-pidelo-773e5.cloudfunctions.net',
+    storageBucket: 'pidelo-773e5.appspot.com',
+    messagingSenderId: '262600937391',
+  }
 };
 
 /*
