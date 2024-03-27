240961fcd758e1f5cf720fca5154fa736d0e9cd5
Rene Arias <rene_811@hotmail.com>
2024-03-26 23:33:09 -0500

    update to angular 11

--- projects/arxis/image-processor/src/lib/image-processor.component.spec.ts
+++ projects/arxis/image-processor/src/lib/image-processor.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ImageProcessorComponent } from './image-processor.component';
 
@@ -6,7 +6,7 @@ describe('ImageProcessorComponent', () => {
   let component: ImageProcessorComponent;
   let fixture: ComponentFixture<ImageProcessorComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ImageProcessorComponent ]
     })
